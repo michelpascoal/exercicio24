e10c8653ed16ba96710336476c0a782289859133
Michel Pascoal Castagnara - 10724258193 <10724258193@ulife.com.br>
2025-04-07 20:34:39 -0300

    Primeiro commit do projeto Java

---
+++ src/main/java/com/mycompany/exercicio24/Exercicio24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio24;
+
+/**
+ *
+ * @author 10724258193
+ */
+public class Exercicio24 {
+
+    public static void main(String[] args) {
+        int i = 14;
+        do{
+            System.out.println (i);
+            i = i + 2;
+        }while (i <23);
+    }
+}
