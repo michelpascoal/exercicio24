e9b56a79c4740d84bbc9f5e037b3ec92e38226f8
Michel Pascoal Castagnara - 10724258193 <10724258193@ulife.com.br>
2025-04-07 21:36:09 -0300

    Atualização do Exercicio24 com código final

--- src/main/java/com/mycompany/exercicio24/Exercicio24.java
+++ src/main/java/com/mycompany/exercicio24/Exercicio24.java
@@ -1,13 +1,17 @@
-
 package com.mycompany.exercicio24;
+
+/*
+ * Escreva um algoritmo que mostre todos os números pares entre 13 e 23
+ * usando do..while.
+ */
 
 public class Exercicio24 {
 
     public static void main(String[] args) {
-        int i = 14;
-        do{
-            System.out.println (i);
+        int i = 14; // Primeiro número par depois de 13
+        do {
+            System.out.println(i);
             i = i + 2;
-        }while (i <23);
+        } while (i <= 22); // Último número par antes de 23
     }
 }
